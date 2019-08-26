fc2e6a1e1be9e543698ba054672a68da8b8963ff
Mark Buie <markebuie@gmail.com>
2019-08-26 16:40:41 -0700

    Dgrid now uses Memory store.

--- markos/_WindowContainer.js
+++ markos/_WindowContainer.js
@@ -6,11 +6,16 @@ define([
     "dijit/_WidgetBase",
     "dijit/_TemplatedMixin",
     "dojo/text!./_WindowContainer/templates/_WindowContainer.html",
-    "dgrid/Grid",
+    "dgrid/OnDemandGrid",
     "dgrid/Keyboard",
     "dgrid/Selection",
+    "dgrid/extensions/ColumnHider",
+    "dgrid/extensions/DijitRegistry",
+    "dgrid/extensions/ColumnResizer",
+    "dstore/Memory",
     "require"
-], function(dom, on, declare, lang, _WidgetBase, _TemplatedMixin, template, Grid, Keyboard, Selection, require) {
+], function(dom, on, declare, lang, _WidgetBase, _TemplatedMixin, template, OnDemandGrid, Keyboard, Selection,
+            ColumnHider, DijitRegistry, ColumnResizer, Memory, require) {
 
     return declare([_WidgetBase, _TemplatedMixin], {
         templateString: template,
@@ -42,16 +47,27 @@ define([
         loadFileTree: function(tree) {
           let columns = tree.headers;
           let data = tree.data;
-          let CustomGrid = declare([ Grid, Keyboard, Selection ]);
+          let CustomGrid = declare([ OnDemandGrid, Keyboard, Selection, DijitRegistry, ColumnResizer , ColumnHider]);
+          let store = new Memory({idProperty: "name", data: data});
 
           let grid = new CustomGrid({
+              collection: store,
               columns: columns,
               // for Selection; only select a single row at a time
               selectionMode: 'single',
               // for Keyboard; allow only row-level keyboard navigation
-              cellNavigation: false
+              cellNavigation: false,
+              // for ColumnResizer; adjusts the last column's width to "auto"
+              // adjustLastColumn: true
           }, this.windowContentNode );
-          grid.renderArray(data);
+          grid.startup();
+
+          grid.on('.dgrid-row:click', function (event) {
+            let row = grid.row(event);
+            console.log('Row clicked:', row);
+          });
+
+          //TODO: Add event listener for column resize that updates the system file.
         },
 
         closeWindow() {
