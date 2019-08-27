32e7e50bae07e375adaad5f4fd7776ac8dffd842
Mark Buie <markebuie@gmail.com>
2019-08-26 21:38:12 -0700

    Merging of software templates are now handled by _SoftwareContainer.

--- markos/FileManager.js
+++ markos/FileManager.js
@@ -1,7 +1,6 @@
 define([
     "dojo/dom",
     "dojo/on",
-    "dojo/query",
     "dojo/_base/declare",
     "dojo/_base/lang",
     "dijit/_WidgetBase",
@@ -14,65 +13,30 @@ define([
     "dgrid/extensions/DijitRegistry",
     "dgrid/extensions/ColumnResizer",
     "dstore/Memory",
-    "dojox/xml/parser",
-    "markos/_SoftwareContainer",
-    "require"
-], function(dom, on, query, declare, lang, _WidgetBase, _TemplatedMixin, template, OnDemandGrid, Keyboard, Selection,
-            ColumnHider, DijitRegistry, ColumnResizer, Memory, xmlParser, _SoftwareContainer, require) {
+    "markos/_SoftwareContainer"
+], function(dom, on, declare, lang, _WidgetBase, _TemplatedMixin, template, OnDemandGrid, Keyboard, Selection,
+            ColumnHider, DijitRegistry, ColumnResizer, Memory, _SoftwareContainer) {
 
     return declare([_SoftwareContainer, _WidgetBase, _TemplatedMixin], {
-        // templateString: template,
-        // baseClass: "markos-folder-tree-container",
+        // For _SoftwareContainer; Merges this template into the _SotwareContainer template.
+        softwareTemplate: template,
         configuration: null,
 
-        buildRendering: function () {
-            // Parse template string into xml document using "dojox/xml/parser"
-            let xml = xmlParser.parse(this.templateString);
-            let xmlDoc = xml.documentElement;
-
-            let xml2 = xmlParser.parse(template);
-            let xmlDoc2 = xml2.documentElement;
-
-            // Find target node which should be modified
-            let targetNode = query("div[data-dojo-attach-point='windowContentNode']", xmlDoc)[0];
-
-            // Create new template content using createElement, createAttribute,
-            // setAttributeNode
-            // let newNode = xml.createElement("button");
-
-            // Append content to the xml template
-            targetNode.appendChild(xmlDoc2);
-
-            // Re-set the modified template string
-            this.templateString = xmlParser.innerXML(xmlDoc);
-            console.log(this.templateString);
-
-            this.inherited(arguments);
-        },
-
         postCreate: function () {
-            // Get a DOM node reference for the root of our widget
-            var domNode = this.domNode;
-
             // Run any parent postCreate processes - can be done at any point
             this.inherited(arguments);
         },
 
         startup: function() {
             if (this.configuration) {
-                console.log(this.configuration);
                 this.loadContent(this.configuration)
             }
-
-            let test = query(".markos-folder-container-body", this.templateString);
-            console.log(test)
         },
 
         loadContent: function(config) {
             if (config.files.headers && config.files.data) {
                 this.loadFileTree(config.files)
             }
-
         },
 
         loadFileTree: function(tree) {
--- markos/_SoftwareContainer.js
+++ markos/_SoftwareContainer.js
@@ -1,25 +1,54 @@
 define([
     "dojo/dom",
     "dojo/on",
+    "dojo/query",
     "dojo/_base/declare",
     "dojo/_base/lang",
     "dijit/_WidgetBase",
     "dijit/_TemplatedMixin",
     "dojo/text!./_SoftwareContainer/templates/_SoftwareContainer.html",
-    "require"
-], function(dom, on, declare, lang, _WidgetBase, _TemplatedMixin, template, require) {
+    "dojox/xml/parser",
+], function(dom, on, query, declare, lang, _WidgetBase, _TemplatedMixin, template, xmlParser) {
 
     return declare([_WidgetBase, _TemplatedMixin], {
         templateString: template,
         baseClass: "markos-file-manager",
+        softwareTemplate: null,
+
+        buildRendering: function () {
+
+            // Used to merger a custom widgit template into this template at data-dojo-attach-point='windowContentNode'
+            this.loadTemplateString(this.softwareTemplate);
+
+            this.inherited(arguments);
+        },
 
         postCreate: function () {
+
             // Run any parent postCreate processes - can be done at any point
             this.inherited(arguments);
         },
 
         startup: function() {
             
+        },
+
+        loadTemplateString: function(template) {
+            let xml = xmlParser.parse(this.templateString);
+            let xmlDoc = xml.documentElement;
+
+            let xml2 = xmlParser.parse(template);
+            let xmlDoc2 = xml2.documentElement;
+
+            // Find target node which should be modified
+            let targetNode = query("div[data-dojo-attach-point='windowContentNode']", xmlDoc)[0];
+
+            // Append content to the xml template
+            targetNode.appendChild(xmlDoc2);
+
+            // Re-set the modified template string
+            this.templateString = xmlParser.innerXML(xmlDoc);
+
         },
 
         closeWindow() {
