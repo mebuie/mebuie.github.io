7de5e573fb1fc4e44ca98df6b3334576b349bfcf
Mark Buie <markebuie@gmail.com>
2019-11-02 16:31:56 -0700

    formatters appended to system.json column properties

--- markos/FileManager.js
+++ markos/FileManager.js
@@ -29,12 +29,6 @@ define([
 
         postCreate: function () {
 
-            this.gridColumns = [
-                {"field": "label", "label": "Name", "unhidable": true, "hidden": false, "width": 150, "formatter": this.gridFormater},
-                {"field": "type", "label": "Type", "unhidable": false, "hidden": true, "width": 50},
-                {"field": "desc", "label": "Description", "unhidable": false, "hidden": false, "width": 175}
-            ]
-
             // Run any parent postCreate processes - can be done at any point
             this.inherited(arguments);
         },
@@ -47,22 +41,22 @@ define([
         },
 
         loadFolderContent: function() {
-            // Get all of the root (Desktop) items in system.json.
+            // Get all of the root (parent = null) items in system.json.
             let folderContent = this.fileSystemStore.filter(
                 new Filter().eq('parent', this.folderId));
 
-            // Get the columns from the widget registry.
-            // let fileManagerRegistry = [];
-            // this.widgetStore.filter({name: "FileManager"}).forEach( function(e) {
-            //     fileManagerRegistry.push(e)
-            // });
-            // let columns = fileManagerRegistry[0].properties.grid.headers;
+            // Get the column parameters from the widget registry.
+            this.parameters = this.getColumnParameters();
+
+            // Appen formatters to column parameters 
+            this.columns = this.appendFormatter();
 
             let CustomGrid = declare([ OnDemandGrid, Keyboard, Selection, DijitRegistry, ColumnResizer , ColumnHider]);
 
+            // Create grid (worlds worst comment). 
             let grid = new CustomGrid({
                 collection: folderContent,
-                columns: this.gridColumns,
+                columns: this.columns,
                 // for Selection; only select a single row at a time
                 selectionMode: 'single',
                 // for Keyboard; allow only row-level keyboard navigation
@@ -72,38 +66,60 @@ define([
             }, this.fileManagerNode );
             grid.startup();
 
+            // Add event listern to grid rows.
             grid.on('.dgrid-row:click', lang.hitch(this, function (event) {
                 this.openItem(grid.row(event));
             }));
 
-            //TODO: Add event listener for column resize that updates the system file.
+            //TODO: Add event listener for grid that updates system.json with parameters.
         },
 
-        openItem: function (item) {
-            // Opens a child file or folder on click.
-
-            //Check Widget Registry for file type
-            let result = [];
-            this.widgetStore.filter({type: item.data.type}).forEach( function(e) {
-                result.push(e)
+        /**
+         * Gets the dgrid column parameters from system.json. 
+         * 
+         * @return {dgrid object}      A dgrid column object. 
+         */
+        getColumnParameters: function() {
+            let fileManagerRegistry = [];
+            this.widgetStore.filter({name: "FileManager"}).forEach( function(e) {
+                fileManagerRegistry.push(e)
             });
-
-            // Get parameters from fileSystem
-            let parameters = Object;
-            if (item.data.parameters) {
-                parameters = item.data.parameters
-            }
-
-            if (result[0].uri) {
-                require([ result[0].uri ], function(LoadedWidget){
-                    let newWidget = new LoadedWidget(parameters);
-                    // Run new widget:
-                    newWidget.placeAt(window.markos.desktopNode);
-                    newWidget.startup();
-                });
-            }
+            return  fileManagerRegistry[0].properties.grid.headers;
         },
 
+        /**
+         * Appends a formatter method for columns that support ${} string replacement. 
+         * 
+         * Methods can't be stored in json, so we append them here. 
+         *  
+         * @return {dgrid object}      A dgrid column ojbect. 
+         */
+        appendFormatter: function() {
+
+            this.parameters.forEach( lang.hitch(this, function(item) {
+
+                switch ( item.label.toLowerCase()) {
+                    case "name":
+                        item.formatter = this.gridFormater
+                        break;
+                    
+                    case "description":
+                        item.formatter = this.gridFormater
+                
+                    default:
+                        break;
+                }
+            }));
+
+            return this.parameters
+
+        },
+
+        /**
+         * Replaces ${some text} in a string with inner text. 
+         * @param  {string} data a string containing ${some text}
+         * @return {string}      a string with ${some text} replace. 
+         */
         gridFormater: function(data) {
             const regex = /\${(.*)}/gm;
             let m;
@@ -118,8 +134,35 @@ define([
             }
 
             return data
-        }
+        },
 
+        /**
+         * Opens a system.json item displayed in the FileManager widget.  
+         * @param  {Item Object} item An item object from system.json.
+         */
+        openItem: function (item) {
+            // Check Widget Registry for item type
+            let result = [];
+            this.widgetStore.filter({type: item.data.type}).forEach( function(e) {
+                result.push(e)
+            });
+
+            // Get item object parameters.
+            let parameters = Object;
+            if (item.data.parameters) {
+                parameters = item.data.parameters
+            }
+
+            // If the item object has an associated widget (uri), then open the widget.
+            if (result[0].uri) {
+                require([ result[0].uri ], function(LoadedWidget){
+                    let newWidget = new LoadedWidget(parameters);
+                    // Run new widget:
+                    newWidget.placeAt(window.markos.desktopNode);
+                    newWidget.startup();
+                });
+            }
+        }
 
     });
 
